8ae024f91e5cf0f69dd8eed4721a0a87920aa246
gidproject2022 <gidproject2022@gmail.com>
2021-12-11 22:19:57 +0900

    hello

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { CalendarList, LocaleConfig } from "react-native-calendars";
+import moment from "moment";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+LocaleConfig.locales["ru"] = {
+  monthNames: [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+  ],
+  monthNamesShort: [
+    "Янв",
+    "Фев",
+    "Мар",
+    "Апр",
+    "Май",
+    "Июн",
+    "Июл",
+    "Авг",
+    "Сен",
+    "Окт",
+    "Ноя",
+    "Дек",
+  ],
+  dayNames: [
+    "воскресенье",
+    "понедельник",
+    "вторник",
+    "среда",
+    "четверг",
+    "пятница",
+    "суббота",
+  ],
+  dayNamesShort: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+  today: "Сегодня",
+};
+LocaleConfig.defaultLocale = "ru";
+
+const dates = [
+  "2021-12-13",
+  "2021-12-14",
+  "2021-12-15",
+  "2021-12-16",
+  "2021-12-17",
+  "2021-12-18",
+  "2021-12-19",
+  "2021-12-20",
+];
+var dates2 = dates.reduce((c, v) =>
+  Object.assign(c, {
+    [v]: {
+      disabled: true,
+      startingDay: true,
+      color: "red",
+      endingDay: true,
+      disabled: true,
+    },
+  })
+);
+console.log(dates2);
+class App extends React.Component {
+  state = {
+    markedDates: {},
+    startDate: "",
+  };
+
+  onDayPress = (day) => {
+    let markedDates = {};
+    markedDates[day.dateString] = {
+      color: "#00B0BF",
+      textColor: "#FFFFFF",
+    };
+    this.setState({
+      markedDates: markedDates + dates,
+    });
+  };
+  render() {
+    return (
+      <CalendarList
+        monthFormat={"MMMM yyyy"}
+        markedDates={this.state.markedDates}
+        pastScrollRange={0}
+        // Max amount of months allowed to scroll to the future. Default = 50
+        futureScrollRange={1}
+        // Enable or disable scrolling of calendar list
+        scrollEnabled={true}
+        minDate={new Date()}
+        onDayPress={this.onDayPress}
+      />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
